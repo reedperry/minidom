6b89f0f287fb382abf31a67bc7bb24ac9148f8bd
Reed Perry <reedperry@users.noreply.github.com>
2020-07-09 21:29:56 -0600

    Rewriting parser, early stages

--- parser.js
+++ parser.js
@@ -1,4 +1,7 @@
 
+const elementSplit = /(?:\s|>)/;
+const attributeMatch = /(\[.+=".+"\])/;
+
 // TODO Handling for children/siblings/grouping
 module.exports = class MiniDOMParser {
 
@@ -7,18 +10,30 @@ module.exports = class MiniDOMParser {
       throw new Error('Invalid element string: Empty/missing');
     }
 
-    let elements = elemStr.split('>');
+    let elements = elemStr.split(elementSplit).map(el => el.trim());
+
+    //let elements = elemStr.split('>');
     if (elements.length > 1) {
       // Future...
-      for (const e of elements) {
-        if (e.includes('+')) {
-          console.log('sibling elements:', e.split('+'));
+      for (let i = 0; i < elements.length; i++) {
+        if (elements[i].includes('+')) {
+          elements[i] = elements[i].split('+');
         }
       }
 
-      return elements.reduceRight((children, str, i) => {
-        return [this.parseSingle(str, children)];
-      }, []);
+        return elements.reduce((tree, currentElement) => {
+          if (tree) {
+            if (!Array.isArray(currentElement)) {
+              tree.children.push(this.parseSingle(currentElement));
+            } else {
+              // FIXME Tree setup is really not implemented yet...
+              tree.children.push(this.parseSingle(currentElement[0]));
+            }
+            return tree;
+          }
+
+          return this.parseSingle(currentElement);
+        }, null);
     }
 
     return this.parseSingle(elemStr);
@@ -29,11 +44,20 @@ module.exports = class MiniDOMParser {
       throw new Error('Invalid element string: Empty/missing');
     }
 
-    let parts = elemStr.split(/([#\.][_a-zA-Z0-9-]+)/).filter(p => !!p);
+    let parts = elemStr.split(/(\[.*?\])|([#\.][_a-zA-Z0-9-]+)/).filter(p => !!p);
 
     if (parts[0].match(/[a-zA-Z]/).index !== 0) {
       throw new Error('Invalid element string: Must start with tag name');
     }
+
+    const attributes = [];
+    for (const part of parts) {
+      if (part.match(attributeMatch) && this.isValidAttribute(part)) {
+        attributes.push(part);
+      }
+    }
+
+    console.log(attributes);
 
     const tagName = parts.shift().match(/[a-zA-Z]+/)[0];
     let id = parts.find(p => p.startsWith('#'));
@@ -47,9 +71,21 @@ module.exports = class MiniDOMParser {
     return {
       children,
       classes,
+      attributes,
       id,
       tagName
     };
+  }
+
+  static isValidAttribute(attrString) {
+    if (!attrString) {
+      return false;
+    }
+    const parts = attrString.split('=');
+    if (parts.length !== 2) {
+      return false;
+    }
+    return true;
   }
 
   static isClass(divider) {
