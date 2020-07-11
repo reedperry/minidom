8b292e9aaa6ef6c37d74ee0a0d0802eb6ec4a46c
Reed Perry <reedperry@users.noreply.github.com>
2020-07-11 08:24:37 -0600

    Parser: Many fixes/improvements to parsing input strings, and continuing refactoring.

--- parser.js
+++ parser.js
@@ -1,119 +1,139 @@
+const validTagName = /^[a-zA-Z]+(?:[-a-zA-Z][a-zA-Z]+)*$/
+const elementListSplit = /(?:\s|>)/
+const elementPartsSplit = /(\[.*?\])|([#\.][_a-zA-Z0-9-]+)/
+const classMatch = /^[_a-zA-Z0-9-]+$/
+const roughAttributeMatch = /(\[.*\])/
+const attributeMatch = /^\[.+=(?:"|').+(?:"|')\]$/
 
-const elementSplit = /(?:\s|>)/;
-const roughAttributeMatch = /(\[.*\])/;
-const attributeMatch = /(\[.+=".+"\])/;
+const isTruthy = (x) => !!x
 
-// TODO Handling for children/siblings/grouping
+// TODO Handling for multiplying?
 module.exports = class MiniDOMParser {
-
   static parse(elemStr) {
     if (!elemStr) {
-      throw new Error('Invalid element string: Empty/missing');
+      throw new Error('Invalid element string: Empty/missing')
     }
 
-    let elements = elemStr.split(elementSplit).map(el => el.trim());
+    let elements = elemStr.split(elementListSplit).map((el) => el.trim())
 
-    //let elements = elemStr.split('>');
     if (elements.length > 1) {
-      // Future...
       for (let i = 0; i < elements.length; i++) {
         if (elements[i].includes('+')) {
-          elements[i] = elements[i].split('+');
+          elements[i] = elements[i].split('+')
         }
       }
 
-        return elements.reduce((tree, currentElement) => {
-          if (tree) {
-            if (!Array.isArray(currentElement)) {
-              tree.children.push(this.parseSingle(currentElement));
-            } else {
-              // FIXME Tree setup is really not implemented yet...
-              tree.children.push(this.parseSingle(currentElement[0]));
-            }
-            return tree;
+      return elements.reduce((tree, currentElement) => {
+        if (tree) {
+          if (!Array.isArray(currentElement)) {
+            tree.children.push(this.parseSingle(currentElement))
+          } else {
+            // FIXME Tree setup is really not implemented yet...
+            tree.children.push(this.parseSingle(currentElement[0]))
           }
+          return tree
+        }
 
-          return this.parseSingle(currentElement);
-        }, null);
+        return this.parseSingle(currentElement)
+      }, null)
     }
 
-    return this.parseSingle(elemStr);
+    return this.parseSingle(elemStr)
   }
 
-  static parseSingle(elemStr, children = []) {
-    if (!elemStr) {
-      throw new Error('Invalid element string: Empty/missing');
+  static parseSingle(input, children = []) {
+    if (!input) {
+      throw new Error('Invalid element string: Empty/missing')
     }
 
-    let parts = elemStr.split(/(\[.*?\])|([#\.][_a-zA-Z0-9-]+)/).filter(p => !!p);
+    let parts = input.split(elementPartsSplit).filter(isTruthy)
 
-    if (parts[0].match(/[a-zA-Z]/).index !== 0) {
-      throw new Error('Invalid element string: Must start with tag name');
+    const tagName = parts[0] || null
+    const tagNameMatch = !!tagName && tagName.match(validTagName)
+    console.log(tagNameMatch)
+    if (!tagNameMatch || tagNameMatch.index !== 0) {
+      // TODO Define elsewhere
+      throw new Error('Invalid element string: Must start with tag name')
     }
 
-    const attributes = [];
-    for (const part of parts) {
-      if (part.match(roughAttributeMatch)) {
-        const attribute = this.parseAttribute(part);
-        if (attribute) {
-          attributes.push(attribute);
-        }
-      }
-    }
+    // Discard tag name from parts list
+    parts.shift()
 
-    const tagName = parts.shift().match(/[a-zA-Z]+/)[0];
-    let id = parts.find(p => p.startsWith('#'));
+    let id = parts.find((p) => p.startsWith('#'))
     if (id) {
-      id = id.substring(1);
+      id = id.substring(1)
     }
+
     const classes = parts
-      .filter(p => p.startsWith('.') && p.substring(1).match(/[_a-zA-Z0-9-]/))
-      .map(p => p.substring(1));
+      .filter((p) => p.startsWith('.') && p.substring(1).match(classMatch))
+      .map((p) => p.substring(1))
+
+    const attributes = this.parseAttributes(parts)
+
+    console.log('Input:', input)
+    console.table({
+      children,
+      classes,
+      attributes,
+      id,
+      tagName,
+    })
 
     return {
       children,
       classes,
       attributes,
       id,
-      tagName
-    };
+      tagName,
+    }
+  }
+
+  static parseAttributes(inputParts) {
+    const attributes = []
+    for (const part of inputParts) {
+      if (part.match(roughAttributeMatch)) {
+        const attribute = this.parseAttribute(part)
+        if (attribute) {
+          attributes.push(attribute)
+        }
+      }
+    }
   }
 
   static parseAttribute(attrString) {
     if (!attrString) {
-      return null;
+      return null
     }
 
     if (!attrString.match(attributeMatch)) {
-      return null;
+      return null
     }
 
-    const strippedAttrString = attrString.substring(1, attrString.length - 1);
+    const strippedAttrString = attrString.substring(1, attrString.length - 1)
 
-    const [name, rawValue] = strippedAttrString.split('=');
+    const [name, rawValue] = strippedAttrString.split('=')
     if (!name || !rawValue) {
-      return null;
+      return null
     }
 
-    const value = rawValue.substring(1, rawValue.length - 1);
+    const value = rawValue.substring(1, rawValue.length - 1)
 
     console.log({
       name,
-      value
-    });
+      value,
+    })
 
     return {
       name,
-      value
-    };
+      value,
+    }
   }
 
   static isClass(divider) {
-    return divider === '.';
+    return divider === '.'
   }
 
   static isId(divider) {
-    return divider === '#';
+    return divider === '#'
   }
-
 }
