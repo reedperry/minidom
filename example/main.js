6d5c650eb5c97099d5f0a39b84b374f4149fc022
Reed Perry <reedperry@users.noreply.github.com>
2020-07-28 22:10:11 -0600

    Add timeUtils and use in example page

--- example/main.js
+++ example/main.js
@@ -1,126 +1,136 @@
-'use strict';
-
-//import { minidom as md } from '../minidom';
+'use strict'
 
 const blue = {
-  background: 'navy'
-};
+  background: 'navy',
+}
 const green = {
-  background: 'darkgreen'
-};
+  background: 'darkgreen',
+}
 const red = {
-  background: 'darkred'
-};
+  background: 'darkred',
+}
 const yellow = {
-  background: 'yellow'
-};
+  background: 'yellow',
+}
 const gray = {
-  background: 'gray'
-};
+  background: 'gray',
+}
 
 const transition = {
   transition: 'background-color .25s ease-in',
-  '-webkit-transition': 'background-color .25s ease-in'
-};
+  '-webkit-transition': 'background-color .25s ease-in',
+}
 const tall = {
   height: '200px',
   width: '100px',
   display: 'inline-block',
-  margin: '2px'
-};
+  margin: '2px',
+}
 const wide = {
   height: '100px',
   width: '200px',
   display: 'inline-block',
-  margin: '2px'
-};
+  margin: '2px',
+}
 const square = {
   height: '150px',
   width: '150px',
   display: 'inline-block',
   margin: 0,
-  padding: 0
-};
+  padding: 0,
+}
 
-const app = md.create('div#app');
+const smallSquare = {
+  height: '30px',
+  width: '30px',
+  display: 'inline-block',
+  margin: 0,
+  padding: 0,
+}
 
-const header = md.build('div.header')
-  .withStyle({ backgroundColor: 'lightgray' })
-  .render();
+const app = md.create('div#app')
 
-const attrs = {
-  href: 'http://google.com',
-  target: '_blank'
-};
+const header = md.build('div.header').withStyle({ backgroundColor: '#ddd' }).render()
+
+const linkData = [
+  { name: 'MDN', href: 'https://developer.mozilla.org' },
+  { name: 'minidom', href: 'https://github.com/reedperry/minidom' },
+  { name: 'Spotify', href: 'https://open.spotify.com' },
+]
+
+const linkAttrs = {
+  target: '_blank',
+}
 
 const linkStyle = {
-  margin: '5px',
-  display: 'inline-block'
-};
+  fontSize: '1.6rem',
+  margin: '0.75rem',
+  display: 'inline-block',
+}
 
-const links = ['Home', 'About', 'Archive'].map(function(text) {
-  const linkContainer = md.build('div.link').withStyle(linkStyle).render();
+const links = linkData.map(function (linkDefinition) {
+  const linkContainer = md.build('div.link').withStyle(linkStyle).render()
   md.append(
-    md.build('a').withText(text).withAttrs(attrs).render(),
+    md
+      .build('a')
+      .withText(linkDefinition.name)
+      .withAttrs({
+        ...linkAttrs,
+        href: linkDefinition.href,
+      })
+      .render(),
     linkContainer
-  );
-  return linkContainer;
-});
+  )
+  return linkContainer
+})
 
-links.forEach(function(link) {
-  md.append(link, header);
-});
+links.forEach(function (link) {
+  md.append(link, header)
+})
 
-md.append(header, app);
+md.append(header, app)
 
 const spaced = {
-  margin: '5px'
-};
+  margin: '10px',
+}
 
-const container = md.create('div#container');
-const colors = [blue, green, red, yellow, gray];
+const container = md.create('div#container')
+const colors = [blue, green, red, yellow, gray]
 for (let i = 0; i < 5; i++) {
-  const box = md.build('div')
+  const box = md
+    .build('div')
     .withStyle(colors[i])
     .withStyle(square)
     .withStyle(spaced)
-    .on('mouseenter', function() {
-      this.style.opacity = 0.3;
+    .on('mouseenter', function () {
+      this.style.opacity = 0.4
     })
-    .on('mouseleave', function() {
-      this.style.opacity = 1;
+    .on('mouseleave', function () {
+      this.style.opacity = 1
     })
-    .render();
+    .render()
 
-  md.append(box, container);
+  md.append(box, container)
 }
 
-md.append(container, app);
+md.append(container, app)
 
-// const els = []
-// for (let i = 0; i < 40; i++) {
-//   els.push(
-//     md.build('div.disco')
-//       .withStyle(square)
-//       .withStyle(transition)
-//       .withStyle(red)
-//       // .withText('Reed')
-//       .render()
-//   )
-// }
-// els.forEach(el => app.appendChild(el))
+const els = []
+for (let i = 0; i < 40; i++) {
+  els.push(md.build('div.disco').withStyle(smallSquare).withStyle(transition).withStyle(red).render())
+}
+els.forEach((el) => md.append(el, app))
 
-md.mount(app, document.body);
+md.mount(app, document.body)
 
-// const styles = [red, blue, green, yellow, gray]
+const styles = [red, blue, green, yellow, gray]
 
-// window.setInterval(() => {
-//   const divs = document.querySelectorAll('div.disco')
-//   for (let d of divs) {
-//     Object.assign(
-//       d.style,
-//       styles[Math.floor(Math.random() * styles.length)],
-//       transition
-//     )
-//   }
-// }, 250)
+let intervalId = timeUtils.everyMs(250, (i) => {
+  const divs = document.querySelectorAll('div.disco')
+  for (let d of divs) {
+    Object.assign(d.style, styles[Math.floor(Math.random() * styles.length)], transition)
+  }
+  if (i > 15) {
+    clearInterval(intervalId)
+  }
+})
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import { minidom } from './minidom';
+import * as timeUtils from './timeUtils';
 window.md = minidom;
+window.timeUtils = timeUtils;
 
 // export { minidom } from './minidom';
+// export { afterMs, afterSeconds, everyMs, everySeconds } from './timeUtils';
